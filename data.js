84cefba80a44bd873606cefa13da96271e02beac
Daniel <daniel.llanos@globant.com>
2018-08-02 02:37:29 -0300

    test data

---
+++ data.js
@@ -0,0 +1,68 @@
+var data = [
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTATGT",
+            "AGAAGG",
+            "CCCCTA",
+            "TCYYYY"
+        ],
+        adnType: "mutant"
+    },
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTATGT",
+            "AGAAGG",
+            "CCCCTA",
+            "TEYYYY"
+        ],
+        adnType: "mutant"
+    },
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTATGT",
+            "AGAAGG",
+            "CRCCTA",
+            "TEYYYY"
+        ],
+        adnType: "human"
+    },
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTAT4T",
+            "AGAAGG",
+            "CRCCTA",
+            "TEYYYY"
+        ],
+        adnType: "human"
+    },
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTAT4T",
+            "AGAAGG",
+            "CRCCGA",
+            "TEYYYY"
+        ],
+        adnType: "human"
+    },
+    {
+        adn: [
+            "ETGCGA",
+            "CAGTTC",
+            "TTAT4T",
+            "AGAAGG",
+            "CTCCGA",
+            "TEYYYY"
+        ],
+        adnType: "human"
+    }
+];
